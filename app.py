19f24b0616c4b51c7692303017f4e9057bb39c71
Krioha <93811161+Krioha@users.noreply.github.com>
2022-11-01 17:44:16 +0700

    comit2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 
 app = Flask(__name__)
 
-def predict(features,jenis,y_test):
+def metric(features,jenis,y_test):
     
     if (jenis=='Ada'):
         pickled_model = pickle.load(open('modelAda.pkl', 'rb'))
@@ -21,6 +21,19 @@ def predict(features,jenis,y_test):
         y_pred=pickled_model.predict(features)
         return metrics.accuracy_score(y_test.values,y_pred),metrics.precision_score(y_test.values,y_pred,average='weighted'),metrics.recall_score(y_test.values,y_pred,average='weighted')
     
+def classification(features,jenis):
+    if (jenis=='Ada'):
+        pickled_model = pickle.load(open('modelAdaNS.pkl', 'rb'))
+        features['Prediksi']=pickled_model.predict(features)
+        return features
+    elif (jenis=='BC'):
+        pickled_model = pickle.load(open('modelBcNS.pkl', 'rb'))
+        features['Prediksi']=pickled_model.predict(features)
+        return features
+    elif (jenis=='HGBC'):
+        pickled_model = pickle.load(open('modelHgbcNS.pkl', 'rb'))
+        features['Prediksi']=pickled_model.predict(features)
+        return features
 
     
 @app.route('/', methods=['GET','POST'])
@@ -28,20 +41,30 @@ def index():
     if request.method=='GET':
         return render_template('index.html')
     elif request.method=='POST':
-        jenis= request.form["jenis"]
-        csv_file=request.files.get("file")
-        df=pd.read_csv(csv_file)
-        X_test=df.drop(columns='AQI')
-        scaler=StandardScaler()
-        X_test=scaler.fit_transform(X_test)
-        y_test=df['AQI']
+        fungsi=request.form["fungsi"]
+        if fungsi=='performansi':
+            jenis= request.form["jenis"]
+            csv_file=request.files.get("file")
+            df=pd.read_csv(csv_file)
+            X_test=df.drop(columns='AQI')
+            scaler=StandardScaler()
+            X_test=scaler.fit_transform(X_test)
+            y_test=df['AQI']
 
 
-        akurasi,presisi,recall = predict(X_test,jenis,y_test)
-        return render_template('Result.html', 
+            akurasi,presisi,recall = metric(X_test,jenis,y_test)
+            return render_template('Result.html', 
                                 akurasi=akurasi,
                                 presisi=presisi,
                                 recall=recall)
+        elif fungsi=='deteksi':
+            jenis= request.form["jenis"]
+            csv_file=request.files.get("file")
+            X_test=pd.read_csv(csv_file)
+            Data_pred=classification(X_test,jenis)
+            return Data_pred.to_html(classes="table table-striped")
+            #return render_template('Table.html')
+
 
     
     return render_template('index.html')
